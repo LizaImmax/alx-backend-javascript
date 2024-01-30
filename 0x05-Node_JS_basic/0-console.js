5b1b5c96edaf23fe2565010160c518d18d02fb50
LizaImmax <immaxliza@gmail.com>
2024-01-30 16:50:11 +0000

    Console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Elizabeth Muthoni
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
+
